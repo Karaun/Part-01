2540739733df03a6588869073c7965f1a42bcb88
Char.ang <114490290+Karaun@users.noreply.github.com>
2022-10-18 19:16:54 +0800

    Add files via upload

---
+++ 出租车轨迹数据计算/出租车轨迹数据计算/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace 出租车轨迹数据计算
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.计算ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.导出结果ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.车辆状态标识修改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.计算ToolStripMenuItem,
+            this.导出结果ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.设置ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.打开ToolStripMenuItem.Text = "打开";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // 计算ToolStripMenuItem
+            // 
+            this.计算ToolStripMenuItem.Name = "计算ToolStripMenuItem";
+            this.计算ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.计算ToolStripMenuItem.Text = "计算";
+            this.计算ToolStripMenuItem.Click += new System.EventHandler(this.计算ToolStripMenuItem_Click);
+            // 
+            // 导出结果ToolStripMenuItem
+            // 
+            this.导出结果ToolStripMenuItem.Name = "导出结果ToolStripMenuItem";
+            this.导出结果ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.导出结果ToolStripMenuItem.Text = "导出结果";
+            this.导出结果ToolStripMenuItem.Click += new System.EventHandler(this.导出结果ToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtSource);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtResult);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 425);
+            this.splitContainer1.SplitterDistance = 393;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // txtSource
+            // 
+            this.txtSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSource.Location = new System.Drawing.Point(0, 0);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSource.Size = new System.Drawing.Size(393, 425);
+            this.txtSource.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResult.Location = new System.Drawing.Point(0, 0);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(403, 425);
+            this.txtResult.TabIndex = 1;
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            this.帮助ToolStripMenuItem.Click += new System.EventHandler(this.帮助ToolStripMenuItem_Click);
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.车辆状态标识修改ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // 车辆状态标识修改ToolStripMenuItem
+            // 
+            this.车辆状态标识修改ToolStripMenuItem.Name = "车辆状态标识修改ToolStripMenuItem";
+            this.车辆状态标识修改ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.车辆状态标识修改ToolStripMenuItem.Text = "车辆状态标识修改";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "出租车轨迹计算器";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 计算ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 导出结果ToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 车辆状态标识修改ToolStripMenuItem;
+    }
+}
+
---
+++ 出租车轨迹数据计算/出租车轨迹数据计算/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace 出租车轨迹数据计算
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        public string path;
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择你要打开的文件";
+            ofd.Filter = "txt|*.txt|All|*.*";
+            ofd.ShowDialog();
+
+            path = ofd.FileName;
+
+            if (path == "")
+            {
+                return;
+            }
+
+            using (FileStream FsRead = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024 * 1024 * 5];
+                int r = FsRead.Read(buffer, 0, buffer.Length);
+                txtSource.Text = Encoding.UTF8.GetString(buffer, 0, r);
+
+
+            }
+        }
+
+        private void 导出结果ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "请选择保存路径";
+            sfd.Filter = "tx|*.txt|All|*.*";
+            sfd.ShowDialog();
+
+            string outcome = sfd.FileName;
+            if (outcome == "")
+            {
+                return;
+            }
+            using (FileStream FsWrite = new FileStream(outcome, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(txtResult.Text);
+                FsWrite.Write(buffer, 0, buffer.Length);
+
+            }
+                
+        }
+
+
+        List<string> marklist = new List<string>();//创建车辆标识数组
+        List<string> Statelist = new List<string>();//创建运行状态数组
+        List<string> Timelist = new List<string>();//创建时间数组
+        List<string> XCoordinate = new List<string>();//创建X坐标数组
+        List<string> YCoordinate = new List<string>();//创建Y坐标数组
+
+        private void 计算ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                #region 计算过程
+                string Paragraph = null;
+                StreamReader sr = new StreamReader(path, Encoding.UTF8);
+                int Str_length = 0;
+                while ((Paragraph = sr.ReadLine()) != null)
+                {
+                    string[] strCollection = Paragraph.Split(',');
+                    marklist.Add(strCollection[0]);
+                    Statelist.Add(strCollection[1]);
+                    Timelist.Add(strCollection[2]);
+                    XCoordinate.Add(strCollection[3]);
+                    YCoordinate.Add(strCollection[4]);
+                    Str_length++;
+                }
+
+                //创建数组
+                List<double> Year = new List<double>();
+                List<double> month = new List<double>();
+                List<double> Day = new List<double>();
+                List<double> hour = new List<double>();
+                List<double> minute = new List<double>();
+                List<double> second = new List<double>();
+                List<double> X_list = new List<double>();
+                List<double> Y_list = new List<double>();
+
+                List<string> MJD = new List<string>(); //mjd
+                List<string> SPEED = new List<string>(); //mjd
+                List<string> AZIMUTH = new List<string>();//
+
+                int index = 0;
+                for (int i = 0; i < Str_length; i++)
+                {
+                    if (marklist[i] == "T2")
+                    {
+                        Year.Add(Convert.ToDouble(Timelist[i].Substring(0, 4)));
+                        month.Add(Convert.ToDouble(Timelist[i].Substring(4, 2)));
+                        Day.Add(Convert.ToDouble(Timelist[i].Substring(6, 2)));
+                        hour.Add(Convert.ToDouble(Timelist[i].Substring(8, 2)));
+                        minute.Add(Convert.ToDouble(Timelist[i].Substring(10, 2)));
+                        second.Add(Convert.ToDouble(Timelist[i].Substring(12, 2)));
+                        X_list.Add(Convert.ToDouble(XCoordinate[i]));
+                        Y_list.Add(Convert.ToDouble(YCoordinate[i]));
+                        index++;
+
+                    }
+                }
+
+                for (int j = 0; j < index; j++)//进行MJD计算
+                {
+                    //txtResult.Text = Year[j] + " " + month[j] + " " + Day[j] + " " + hour[j] + " " + minute[j] + " " + second[j] + " " + txtResult.Text;
+                    double mjd = -678987 + 367.0 * Year[j];
+                    mjd -= Convert.ToInt32(7.0 / 4.0 * (Year[j] + Convert.ToInt32((month[j] + 9.0) / 12.0)));
+                    mjd += Convert.ToInt32((275.0 * month[j]) / 9.0);
+                    mjd += Day[j] + (hour[j] - 8) / 24.0 + minute[j] / 1440.0 + second[j] / 86400.0;
+                    //MessageBox.Show((second[i] / 86400).ToString());
+                    // MessageBox.Show(n.ToString());
+                    MJD.Add(mjd.ToString("#0.00000"));
+                    //txtResult.Text = txtResult.Text + MJD[j] + "    ";
+
+                }
+
+                double Cumulative_distance = 0.0;
+                for (int k = 1; k < index; k++)//计算速度
+                {
+                    double time1 = (hour[k] + (minute[k] / 60) + (second[k] / 3600));
+                    double time2 = (hour[k - 1] + (minute[k - 1] / 60) + (second[k - 1] / 3600));
+                    double distance = Math.Sqrt((X_list[k] - X_list[k - 1]) * (X_list[k] - X_list[k - 1]) + (Y_list[k] - Y_list[k - 1]) * (Y_list[k] - Y_list[k - 1]));
+                    double speed = distance / (time1 - time2) / 1000;
+                    SPEED.Add(speed.ToString("#0.000"));
+                    Cumulative_distance += distance;
+                    //txtResult.Text = txtResult.Text + SPEED[k - 1] + "    ";
+                }
+
+
+
+                for (int q = 1; q < index; q++)//计算方位角
+                {
+
+                    double dy = (Y_list[q] - Y_list[q - 1]); //定义△Y
+                    double dx = (X_list[q] - X_list[q - 1]); //定义△X
+                    double azimuth = 0.0;
+
+                    if (dx == 0)
+                    {
+                        if (dy > 0)
+                            azimuth = 0.5 * Math.PI;
+                        else
+                        {
+                            azimuth = 1.5 * Math.PI;
+                        }
+                    }
+                    else
+                    {
+                        azimuth = Math.Atan2(dy, dx);
+                        if (dx < 0)
+                        {
+                            azimuth += Math.PI;
+                        }
+                    }
+
+                    if (azimuth < 0)
+                    {
+                        azimuth += 2 * Math.PI;
+                    }
+                    if (azimuth > 2 * Math.PI)
+                    {
+                        azimuth -= 2 * Math.PI;
+                    }
+
+                    azimuth = azimuth * 180 / Math.PI;
+
+
+                    AZIMUTH.Add(azimuth.ToString("#0.000"));
+                    // txtResult.Text = txtResult.Text + AZIMUTH[q - 1] + "    ";
+                }
+                #endregion
+
+
+                #region 输出过程
+                string Start_title = "-------------速度和方位角计算结果---------------" + "\r\n" + "序号——--——时段——————速度——方位角";
+                string End_title = "-------------速度和方位角计算结果---------------";
+
+                string out_come = Start_title + "\r\n";
+                for (int x = 1; x < index; x++)
+                {
+                    if (x < 11)
+                    {
+                        out_come = out_come + "0" + (x - 1) + " ," + MJD[x - 1] + " - " + MJD[x] + " ," + SPEED[x - 1] + " ," + AZIMUTH[x - 1] + "\r\n";
+                    }
+                    else
+                    {
+                        out_come = out_come + (x - 1) + " ," + MJD[x - 1] + " - " + MJD[x] + " ," + SPEED[x - 1] + " ," + AZIMUTH[x - 1] + "\r\n";
+                    }
+                }
+
+                out_come += End_title + "\r\n";
+                double linear_distance = Math.Sqrt((X_list[index - 1] - X_list[0]) * (X_list[index - 1] - X_list[0]) + (Y_list[index - 1] - Y_list[0]) * (Y_list[index - 1] - Y_list[0]));
+                string distance_title = "累积距离：" + (Cumulative_distance / 1000).ToString("#0.000") + " (km)" + "\r\n" + "首位直线距离：" + (linear_distance / 1000).ToString("#0.000") + " (km)";
+                txtResult.Text = out_come + distance_title;
+                #endregion
+            }
+            catch
+            {
+                MessageBox.Show("操作错误", "提示");
+            }
+
+        }
+
+        private void 帮助ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("制作者：昆明理工大学_CZY" + "\r\n" + "数据若有错误需要修改" + "\r\n" + "请对导入数据进行修改","帮助");
+        }
+    }
+}
+
